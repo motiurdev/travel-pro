43c73175b9e00c95f4664285029109a364a657bf
motiurrahmanmisu <motiurrahmanmisu@gmail.com>
2021-10-30 17:35:56 +0600

    add-spinner

--- src/compoments/AddBooking/AddBooking.js
+++ src/compoments/AddBooking/AddBooking.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import useAuth from '../../hooks/useAuth';
+import './AddBooking.css'
 
 const AddBooking = () => {
+    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
+    const { user } = useAuth()
+    const onSubmit = data => {
+        data.email = user?.email;
+        fetch(`http://localhost:5000/addOrder`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert("Added Successfully")
+                    reset()
+                }
+            })
+    };
     return (
-        <div>
-            <h1>Add Booking</h1>
+        <div className="container">
+            <h1 className="header-title text-center py-5">Add Booking</h1>
+            <div className="order-form addForm">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input placeholder="Name" {...register("name")} />
+                    <input placeholder="Price" {...register("price", { required: true })} />
+                    <textarea placeholder="Description" {...register("des")} />
+                    <input placeholder="Image Url" {...register("img")} />
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <input type="submit" value="Place Order" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/compoments/Home/Services/Services.js
+++ src/compoments/Home/Services/Services.js
@@ -1,35 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import "./Services.css"
 
 const Services = () => {
     const [services, setServices] = useState([])
+    const [isLoading, setIsLading] = useState(true)
+
     useEffect(() => {
+        setIsLading(true)
         fetch("http://localhost:5000/allServices")
             .then(res => res.json())
             .then(data => setServices(data))
+            .finally(() => {
+                setIsLading(false)
+            })
     }, [])
 
+
+
     return (
-        <div className="container">
-            <h1 className="text-center py-5 header-title">Our Services</h1>
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-                {
-                    services.map(service => (
-                        <div className="col" key={service._id}>
-                            <div className="card h-100">
-                                <img src={service.img} className="card-img-top service-img" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">{service.name}</h5>
-                                    <p className="card-text">{service?.des.slice(0, 100)}</p>
-                                    <h5 className="mb-4">${service.price}</h5>
-                                    <Link to={`/bookingDetail/${service._id}`} className="regular-btn">Booking now <i className="fas fa-arrow-circle-right"></i></Link>
-                                </div>
-                            </div>
+        <div>
+            {
+                isLoading ? (<Spinner animation="border" variant="success" className="d-block mx-auto mt-5" />) :
+                    (<div className="container">
+                        <h1 className="text-center py-5 header-title">Our Services</h1>
+                        <div className="row row-cols-1 row-cols-md-3 g-4">
+                            {
+                                services.map(service => (
+                                    <div className="col" key={service._id}>
+                                        <div className="card h-100">
+                                            <img src={service.img} className="card-img-top service-img" alt="..." />
+                                            <div className="card-body">
+                                                <h5 className="card-title">{service.name}</h5>
+                                                <p className="card-text">{service?.des.slice(0, 100)}</p>
+                                                <h5 className="mb-4">${service.price}</h5>
+                                                <Link to={`/bookingDetail/${service._id}`} className="regular-btn">Booking now <i className="fas fa-arrow-circle-right"></i></Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
                         </div>
-                    ))
-                }
-            </div>
+                    </div>)
+            }
         </div>
     );
 };
