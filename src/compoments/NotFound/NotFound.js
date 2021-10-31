4051a1872c8447bbdd3ab5c91a1b85d4ec09c530
motiurrahmanmisu <motiurrahmanmisu@gmail.com>
2021-10-31 19:55:13 +0600

    not-found-button

--- src/compoments/NotFound/NotFound.js
+++ src/compoments/NotFound/NotFound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import pageNotFound from '../../images/page-not-found.svg'
 
 const NotFound = () => {
@@ -6,6 +7,7 @@ const NotFound = () => {
         <div className="container text-center py-5">
             <img src={pageNotFound} alt="" className="w-50" />
             <h1 className="header-title py-3">Page Not Found</h1>
+            <Link to="/home" className="regular-btn">Back Home</Link>
         </div>
     );
 };
